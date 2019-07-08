fcedd27378de886c58b4ea5f1b8eb9b6e5f75f1a
kaleidosgu <gjcaoyi@163.com>
2019-07-08 11:24:22 +0800

    添加一些执行操作的逻辑

---
+++ Block/Assets/Scripts/BlockGlobalDefine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGlobalDefine : MonoBehaviour {
+
+    public static string TagBlock = "BlockTag";
+    public static string TagGround = "GroundTag";
+
+    public static int MaxCountsCreated = 20;
+
+    public static int CurrentCountsCreated;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Block/Assets/Scripts/PlayerController.cs
+++ Block/Assets/Scripts/PlayerController.cs
@@ -12,22 +12,24 @@ public class PlayerController : MonoBehaviour {
     public float MoveDiff;
     public float JumpDiff;
     public float TimeNoToSimulateRigid;
-
-    public PlayerController PCZ;
-    public PlayerController PCRZ;
-    public PlayerController PCT;
-    public PlayerController PC7;
-    public PlayerController PCR7;
-    public PlayerController PCSquare;
-    public PlayerController PCBar;
+    
+    public bool NotUpdateExecution;
 
     public List<GameObject> LstPlayerController;
+
+    public float JumpSpeed;
 
     private Rigidbody2D m_rigidBody;
     private bool m_bNoSiulate;
     private float m_fCurrentTimeNoToSimulateRigid;
 
     private SpawnPosition m_spawnPosition;
+
+    private bool m_bBeFixed;
+
+    private bool m_bGenerated;
+
+    private bool m_bCanJump;
     
 	// Use this for initialization
 	void Start () {
@@ -37,21 +39,44 @@ public class PlayerController : MonoBehaviour {
         {
             Debug.Log("SpawnPostion Not Found");
         }
-        
-
+        NotUpdateExecution = false;
+        m_bCanJump = true;
 
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {
+        if (m_bNoSiulate == true)
+        {
+            m_fCurrentTimeNoToSimulateRigid += Time.deltaTime;
+            if (m_fCurrentTimeNoToSimulateRigid >= TimeNoToSimulateRigid)
+            {
+                m_bNoSiulate = false;
+                if (m_rigidBody != null)
+                {
+                    m_rigidBody.simulated = true;
+                }
+            }
+        }
+        if(m_bGenerated == false)
+        {
+            if (Input.GetKeyDown(FixedKeycode) == true)
+            {
+                _generateBlock();
+                m_bGenerated = true;
+            }
+        }
+        if (NotUpdateExecution == true)
+        {
+            return;
+        }
 		if( Input.GetKey(LeftKeycode) == true )
         {
-            //transform.position.Set(transform.position.x + MoveDiff, transform.position.y, transform.position.z);
             transform.position = new Vector3(transform.position.x - MoveDiff, transform.position.y, transform.position.z);
         }
         if( Input.GetKey(RightKeycode) == true )
         {
-            //transform.position.Set(transform.position.x - MoveDiff, transform.position.y, transform.position.z);
             transform.position = new Vector3(transform.position.x + MoveDiff, transform.position.y, transform.position.z);
         }
         if( Input.GetKeyUp(RotationKeycode) == true)
@@ -60,39 +85,128 @@ public class PlayerController : MonoBehaviour {
         }
         if( Input.GetKeyDown(JumpKeycode) == true )
         {
-            transform.position = new Vector3(transform.position.x , transform.position.y + JumpDiff, transform.position.z);
-            m_bNoSiulate = true;
-            m_rigidBody.simulated = false;
-            m_fCurrentTimeNoToSimulateRigid = 0.0f;
+            _jumpProcess();
+            _controlEulerAngle();
         }
-        if( Input.GetKeyDown(FixedKeycode) == true)
+	}
+    private void _controlEulerAngle()
+    {
+        float fCurrentAngle = transform.localEulerAngles.z;
+        float fChangeAngle = 0;
+        if( fCurrentAngle >= 0 && fCurrentAngle < 45 )
         {
-            m_bNoSiulate = false;
-            m_rigidBody.simulated = true;
-            enabled = false;
+            fChangeAngle = 0;
+        }
+        else if (fCurrentAngle > 45 && fCurrentAngle <= 135)
+        {
+            fChangeAngle = 90;
+        }
+        else if (fCurrentAngle > 135 && fCurrentAngle <= 225)
+        {
+            fChangeAngle = 180;
+        }
+        else if (fCurrentAngle > 225 && fCurrentAngle <= 315)
+        {
+            fChangeAngle = 270;
+        }
+        else
+        {
+            fChangeAngle = 0;
+        }
+        transform.localEulerAngles = new Vector3(0, 0, fChangeAngle);
+    }
 
-            int nRanResult = Random.Range(0, LstPlayerController.Count - 1);
+    private void _jumpBlock()
+    {
+        //transform.position = new Vector3(transform.position.x, transform.position.y + JumpDiff, transform.position.z);
+        //m_bNoSiulate = true;
+        //if (m_rigidBody != null)
+        //{
+        //    m_rigidBody.simulated = false;
+        //}
+        //m_fCurrentTimeNoToSimulateRigid = 0.0f;
 
-            if(nRanResult < 0 || nRanResult >6 )
+        //m_bJump = true;
+        _jumpProcess();
+    }
+
+    private void _jumpProcess()
+    {
+        if(m_bCanJump == true)
+        {
+            //float fAddForce = JumpSpeed * Time.deltaTime;
+            float fAddForce = JumpSpeed ;
+            m_rigidBody.AddForce(Vector2.up * fAddForce);
+            m_bCanJump = false;
+        }
+    }
+    private void _destroyRigid()
+    {
+        m_rigidBody = null;
+        Destroy(GetComponent<Rigidbody2D>());
+    }
+    private void _generateBlock()
+    {
+        m_bNoSiulate = false;
+
+        NotUpdateExecution = true;
+        m_bBeFixed = true;
+
+        int nRanResult = Random.Range(0, LstPlayerController.Count - 1);
+
+        if (nRanResult < 0 || nRanResult >= LstPlayerController.Count)
+        {
+            Debug.Log(string.Format("nRanResult[{0}]", nRanResult));
+        }
+        else
+        {
+            if (m_spawnPosition != null)
             {
-                int a = 0;
+                if (LstPlayerController[nRanResult] != null)
+                {
+                    GameObject dummyObj = Instantiate(LstPlayerController[nRanResult], m_spawnPosition.transform.position, Quaternion.identity);
+                    dummyObj.transform.SetParent(transform.parent);
+                    dummyObj.GetComponent<PlayerController>().NotUpdateExecution = false;
+                }
+                else
+                {
+                    Debug.Log(string.Format("LstPlayerController cout {0} nRanResult = {1}", LstPlayerController.Count, nRanResult));
+                }
             }
             else
             {
-                GameObject dummyObj = Instantiate(LstPlayerController[nRanResult], m_spawnPosition.transform.position, Quaternion.identity);
-                dummyObj.transform.SetParent(transform.parent);
-                dummyObj.GetComponent<PlayerController>().enabled = true;
+                Debug.Log("m_spawnPosition is null");
             }
-
         }
-        if(m_bNoSiulate == true)
+        NotUpdateExecution = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (BlockGlobalDefine.CurrentCountsCreated <= BlockGlobalDefine.MaxCountsCreated)
+        //{
+        //    BlockGlobalDefine.CurrentCountsCreated++;
+        //    //if (collision.gameObject.tag == BlockGlobalDefine.TagBlock || collision.gameObject.tag == BlockGlobalDefine.TagGround)
+        //    if (collision.gameObject.tag == BlockGlobalDefine.TagBlock )
+        //    {
+        //        m_rigidBody = null;
+        //        Destroy(GetComponent<Rigidbody2D>());
+        //        NotUpdateExecution = true;
+        //    }
+        //}
+        m_bCanJump = true;
+
+        if(m_bBeFixed == true)
         {
-            m_fCurrentTimeNoToSimulateRigid += Time.deltaTime;
-            if( m_fCurrentTimeNoToSimulateRigid >= TimeNoToSimulateRigid)
+            if (m_rigidBody != null)
             {
-                m_bNoSiulate = false;
                 m_rigidBody.simulated = true;
             }
+            _destroyRigid();
         }
-	}
+    }
 }
